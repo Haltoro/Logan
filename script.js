40e20e37137db3b7c9aea7c7210784857eeab8f8
Logan S <99504554+Haltoro@users.noreply.github.com>
2022-06-07 16:41:03 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+  // Write "HI" here using rectangles
+   rect(90,20,75,250);
+   rect(165,110,65);
+   rect(230,20,75,250);
+   rect(400,20,140,55);
+   rect(435,75,75,140);
+   rect(400,215,140,55);
+  
+}
